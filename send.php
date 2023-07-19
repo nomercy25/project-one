ffb64f1274d5b590678d89c6c5e2bc36bbe552d3
misha1997 <mishaotroshenko2013@gmail.com>
2023-07-19 18:58:25 +0300

    fix

--- send.php
+++ send.php
@@ -1,20 +1,32 @@
 <?
-
 $token = "6313365253:AAEBkQEikK20PisOGeKnXpeQxfS8mFhzJ1E";
+$chatId = "-955308780";
 
-$getQuery = array(
-     "chat_id" 	=> -955308780,
-     "text"  	=> "Новое сообщение из формы",
-     "parse_mode" => "html",
-);
-$ch = curl_init("https://api.telegram.org/bot". $token ."/sendMessage?" . http_build_query($getQuery));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_HEADER, false);
+if ($_POST) {
+     $arr = [
+          'Имя:' => $_POST['firstName'],
+          'Фамилия:' => $_POST['lastName'],
+          'Телефон' => $_POST['phone'],
+          'Почта:' => $_POST['email']
+     ];
 
-$resultQuery = curl_exec($ch);
-curl_close($ch);
+     foreach ($arr as $key => $value) {
+          $txt .= "<b>" . $key . "</b> " . $value . "\n";
+     };
 
-echo $resultQuery;
+     $getQuery = array(
+          "chat_id" => $chatId,
+          'text' => $txt,
+          "parse_mode" => "html",
+     );
 
-?>
+     $ch = curl_init("https://api.telegram.org/bot" . $token . "/sendMessage?" . http_build_query($getQuery));
+     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+     curl_setopt($ch, CURLOPT_HEADER, false);
+
+     $resultQuery = curl_exec($ch);
+     curl_close($ch);
+
+     echo json_encode("ok");
+}
